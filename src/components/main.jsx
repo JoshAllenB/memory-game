1597e22df8b274691b4174301d610fcc2969c3b9
JoshAllenB <batayolajoshuaallen@gmail.com>
2024-05-13 20:57:09 +0800

    removed accidentally

---
+++ src/components/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MemoryGame from "./Api";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MemoryGame />
+  </React.StrictMode>
+);
